1b2cc3cd7c727a397c56cce8323e585ecd29c44f
Ernest <emudjambere@gmail.com>
2025-01-15 18:14:29 -0500

    Project setup.v2

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace OcculusControlle_UI.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
